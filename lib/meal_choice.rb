4ea18c42cbdfb2c8048cd1cac6ea26a78b99ea8a
Mark G <markghaida@gmail.com>
2020-10-27 14:36:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,3 +7,8 @@ require 'pry'
   #puts "A plate of #{protein} with #{veg1} and #{veg2}."
 #end
 
+def meal_choice(veg1, veg2, protein = 'tofu')
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  "A plate of #{protein} with #{veg1} and #{veg2}."
+end
